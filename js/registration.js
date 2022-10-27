24bde701f9cbaed5ce67926eab9812a616bf9c22
saiful542 <saifuliiuc12@gmail.com>
2022-10-27 21:57:14 +0600

    dasd

--- js/registration.js
+++ js/registration.js
@@ -1,1 +1,26 @@
-console.log(document.forms.documen.getg)
+const form = document.forms
+console.log(form)
+
+
+const check = document.querySelector('.check')
+console.log(check)
+
+check.addEventListener('change', (e) => {
+    const pass = document.querySelector('.user-password')
+    const v = pass.value
+    if (check.checked) {
+
+        pass.type = 'text'
+        console.log(pass.type)
+
+
+    }
+
+    else {
+        console.log(v)
+        pass.type = 'password'
+        document.querySelector('.user-password').value = v
+    }
+
+
+})
