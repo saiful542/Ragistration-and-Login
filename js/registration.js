2d188e1d04ddf95d2ffad08e69a190f201c16cb6
saiful542 <saifuliiuc12@gmail.com>
2022-10-31 19:17:31 +0600

    sas

--- js/registration.js
+++ js/registration.js
@@ -5,18 +5,14 @@ const userEmail = document.querySelector('.registration-form .left-side .user-em
 const userPassword = document.querySelector('.registration-form .left-side .user-password')
 const retypePassword = document.querySelector('.registration-form .left-side .retype-password')
 const signupButton = document.querySelector('.registration-form .left-side .signup-button')
-const massage = document.querySelector('.registration-form .left-side .massage')
-const allInputs = document.querySelectorAll('.registration-form .left-side input')
-const regForm = document.querySelectorAll('.registration-form')
 const check = document.querySelector('.checkbox-wrapper .check')
 const checkWrapper = document.querySelector('.left-side .checkbox-wrapper')
 const emailError = document.querySelector('.user-email').parentElement.parentElement.parentElement.lastElementChild
 const nameError = document.querySelector('.first-name').parentElement.parentElement.parentElement.lastElementChild
 const passwordError = document.querySelector('.user-password').parentElement.parentElement.parentElement.lastElementChild
-const confirmError = document.querySelector('.retype-password').parentElement.parentElement.parentElement.lastElementChild
 let validation = false;
 let user;
-
+const alert_svg = `<svg aria-hidden="true" class="stUf5b LxE1Id" fill="currentColor" focusable="false" width="16px" height="16px" viewBox="0 0 24 24" xmlns="https://www.w3.org/2000/svg"><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-2h2v2zm0-4h-2V7h2v6z"></path></svg>`
 
 
 //floating label
@@ -30,7 +26,6 @@ form.addEventListener('change', (e) => {
         }
     }
 })
-
 
 
 // signupButton handle
@@ -65,7 +60,7 @@ signupButton.addEventListener('click', (e) => {
 // checkNameFieldError
 const checkNameFieldError = () => {
     if (!firstName.value && !lastName.value) {
-        nameError.innerHTML = `<div class="error-wrapper"><svg aria-hidden="true" class="stUf5b LxE1Id" fill="currentColor" focusable="false" width="16px" height="16px" viewBox="0 0 24 24" xmlns="https://www.w3.org/2000/svg"><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-2h2v2zm0-4h-2V7h2v6z"></path></svg><p>Enter First and Name</p></div>`
+        nameError.innerHTML = `<div class="error-wrapper">${alert_svg}<p>Enter First and Name</p></div>`
         validation = false;
         firstName.classList.add('input-error-alert')
         firstName.nextElementSibling.classList.add('label-error-alert')
@@ -74,7 +69,7 @@ const checkNameFieldError = () => {
 
     }
     else if (!firstName.value) {
-        nameError.innerHTML = `<div class="error-wrapper"><svg aria-hidden="true" class="stUf5b LxE1Id" fill="currentColor" focusable="false" width="16px" height="16px" viewBox="0 0 24 24" xmlns="https://www.w3.org/2000/svg"><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-2h2v2zm0-4h-2V7h2v6z"></path></svg><p>Enter First Name</p></div>`
+        nameError.innerHTML = `<div class="error-wrapper">${alert_svg}<p>Enter First Name</p></div>`
         validation = false;
         firstName.classList.add('input-error-alert')
         firstName.nextElementSibling.classList.add('label-error-alert')
@@ -83,7 +78,7 @@ const checkNameFieldError = () => {
 
     }
     else if (!lastName.value) {
-        nameError.innerHTML = `<div class="error-wrapper"><svg aria-hidden="true" class="stUf5b LxE1Id" fill="currentColor" focusable="false" width="16px" height="16px" viewBox="0 0 24 24" xmlns="https://www.w3.org/2000/svg"><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-2h2v2zm0-4h-2V7h2v6z"></path></svg><p>Enter Last Name</p></div>`
+        nameError.innerHTML = `<div class="error-wrapper">${alert_svg}<p>Enter Last Name</p></div>`
         validation = false;
         firstName.classList.remove('input-error-alert')
         firstName.nextElementSibling.classList.remove('label-error-alert')
@@ -98,10 +93,7 @@ const checkNameFieldError = () => {
         lastName.classList.remove('input-error-alert')
         firstName.nextElementSibling.classList.remove('label-error-alert')
         lastName.nextElementSibling.classList.remove('label-error-alert')
-
-
     }
-
 }
 
 // checkEmailFieldError
@@ -109,13 +101,13 @@ const checkNameFieldError = () => {
 const checkEmailFieldError = () => {
     const mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
     if (!userEmail.value) {
-        emailError.innerHTML = `<div class="error-wrapper"><svg aria-hidden="true" class="stUf5b LxE1Id" fill="currentColor" focusable="false" width="16px" height="16px" viewBox="0 0 24 24" xmlns="https://www.w3.org/2000/svg"><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-2h2v2zm0-4h-2V7h2v6z"></path></svg><p>Choose a Gmail address</p></div>`
+        emailError.innerHTML = `<div class="error-wrapper">${alert_svg}<p>Choose a Gmail address</p></div>`
         validation = false;
         userEmail.classList.add('input-error-alert')
         userEmail.nextElementSibling.classList.add('label-error-alert')
     }
     else if (!userEmail.value.match(mailformat)) {
-        emailError.innerHTML = `<div class="error-wrapper"><svg aria-hidden="true" class="stUf5b LxE1Id" fill="currentColor" focusable="false" width="16px" height="16px" viewBox="0 0 24 24" xmlns="https://www.w3.org/2000/svg"><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-2h2v2zm0-4h-2V7h2v6z"></path></svg><p>You have entered an invalid email address</p></div>`
+        emailError.innerHTML = `<div class="error-wrapper">${alert_svg}<p>You have entered an invalid email address</p></div>`
         validation = false;
         userEmail.classList.add('input-error-alert')
         userEmail.nextElementSibling.classList.add('label-error-alert')
@@ -126,7 +118,6 @@ const checkEmailFieldError = () => {
         validation = true;
         userEmail.classList.remove('input-error-alert')
         userEmail.nextElementSibling.classList.remove('label-error-alert')
-
     }
 }
 
@@ -135,14 +126,14 @@ const checkPasswordFieldError = () => {
     var passwordFormat = /^[A-Za-z]\w{7,200}$/;
 
     if (!userPassword.value) {
-        passwordError.innerHTML = `<div class="error-wrapper"><svg aria-hidden="true" class="stUf5b LxE1Id" fill="currentColor" focusable="false" width="16px" height="16px" viewBox="0 0 24 24" xmlns="https://www.w3.org/2000/svg"><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-2h2v2zm0-4h-2V7h2v6z"></path></svg><p>Enter a Password</p></div>`
+        passwordError.innerHTML = `<div class="error-wrapper">${alert_svg}<p>Enter a Password</p></div>`
         validation = false;
         userPassword.classList.add('input-error-alert')
         userPassword.nextElementSibling.classList.add('label-error-alert')
     }
 
     else if (!userPassword.value.match(passwordFormat)) {
-        passwordError.innerHTML = `<div class="error-wrapper"><svg aria-hidden="true" class="stUf5b LxE1Id" fill="currentColor" focusable="false" width="16px" height="16px" viewBox="0 0 24 24" xmlns="https://www.w3.org/2000/svg"><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-2h2v2zm0-4h-2V7h2v6z"></path></svg><p>Use 8 characters or more for your password</p></div>`
+        passwordError.innerHTML = `<div class="error-wrapper">${alert_svg}<p>Use 8 characters or more for your password</p></div>`
         validation = false;
         userPassword.classList.add('input-error-alert')
         userPassword.nextElementSibling.classList.add('label-error-alert')
@@ -151,7 +142,7 @@ const checkPasswordFieldError = () => {
     }
 
     else if (!retypePassword.value) {
-        passwordError.innerHTML = `<div class="error-wrapper"><svg aria-hidden="true" class="stUf5b LxE1Id" fill="currentColor" focusable="false" width="16px" height="16px" viewBox="0 0 24 24" xmlns="https://www.w3.org/2000/svg"><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-2h2v2zm0-4h-2V7h2v6z"></path></svg><p>Confirm your pasword</p></div>`
+        passwordError.innerHTML = `<div class="error-wrapper">${alert_svg}<p>Confirm your pasword</p></div>`
         validation = false;
         retypePassword.classList.add('input-error-alert')
         retypePassword.nextElementSibling.classList.add('label-error-alert')
@@ -160,7 +151,7 @@ const checkPasswordFieldError = () => {
     }
 
     else if (userPassword.value != retypePassword.value) {
-        passwordError.innerHTML = `<div class="error-wrapper"><svg aria-hidden="true" class="stUf5b LxE1Id" fill="currentColor" focusable="false" width="16px" height="16px" viewBox="0 0 24 24" xmlns="https://www.w3.org/2000/svg"><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-2h2v2zm0-4h-2V7h2v6z"></path></svg><p>Those passwords didn’t match.Try again.</p></div>`
+        passwordError.innerHTML = `<div class="error-wrapper">${alert_svg}<p>Those passwords didn’t match.Try again.</p></div>`
         validation = false;
         retypePassword.classList.add('input-error-alert')
         retypePassword.nextElementSibling.classList.add('label-error-alert')
@@ -181,15 +172,8 @@ const checkPasswordFieldError = () => {
 
 
 
-
-
-
-
-
-
 //show password
 check.addEventListener('change', (e) => {
-    const passwordValue = userPassword.value
     if (check.checked) {
         userPassword.type = 'text'
         retypePassword.type = 'text'
